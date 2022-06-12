220a4ea49484213c9c2734ace829352beb2e8671
sanghoon <digimon.1740@gmail.com>
2022-06-12 20:44:19 +0900

    초기 세팅

---
+++ issue-service/src/main/kotlin/fastcampus/issueservice/FastcampusIssueServiceApplication.kt
@@ -0,0 +1,11 @@
+package fastcampus.issueservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusIssueServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastcampusIssueServiceApplication>(*args)
+}
